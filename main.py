857a6dd6f61c44af3cf7806a279850665106707b
dmozze <dmozze@yandex.ru>
2023-12-07 20:34:04 +0300

    add statistics

--- main.py
+++ main.py
@@ -8,11 +8,23 @@ backdoor_path = 'backdoors.txt'
 
 start = time.time()
 
+prop_hit = 0
+
+
+
 def send_to_telegram(data):
     token = "6972984435:AAGeBAFCALEoz2SXhHLX-uyyj0HWbVny9l8"
+    print(data)
+    string = str(data)
+    # add \n after each key-value pair
+    string = string.replace(", ", "\n")
+    # remove { and }
+    string = string.replace("{", "")
+    string = string.replace("}", "")
+
     requests.post(
         url='https://api.telegram.org/bot{0}/{1}'.format(token, "sendMessage"),
-        data={'chat_id': 277499288, 'text': data}
+        data={'chat_id': 277499288, 'text': string}
     )
 
 
@@ -28,6 +40,7 @@ def next_backdoor(bitvector):
         else:
             bitvector[i] = -bitvector[i]
     return []
+
 
 backdoors = []
 with open(backdoor_path, 'r') as f:
@@ -51,6 +64,7 @@ def _propagate(assumptions):
     all_assigned = len(literals) >= base_solver.nof_vars()
     return status and (all_assigned or None), assumptions
 
+
 def get_unique_lists(lists):
     unique_lists = []
     for l in lists:
@@ -58,9 +72,12 @@ def get_unique_lists(lists):
             unique_lists.append(l)
     return unique_lists
 
+
+kkk = 0
+
 def merge_backdoors(a, b):
+    global prop_hit
     result = []
-    cnt = 0
     for i in range(len(a)):
         for j in range(len(b)):
             merged = merge_list(a[i], b[j])
@@ -70,8 +87,11 @@ def merge_backdoors(a, b):
                 status, res = _propagate(merged)
                 if status is None:
                     result.append(merged)
+                else:
+                    prop_hit = prop_hit + 1
     result = filter(lambda x: x != [], result)
     return list(result)
+
 
 def merge_list(a, b):
     result_set = set(a + b)
@@ -108,30 +128,38 @@ for i in range(len(backdoors)):
     decart.append(results)
 
 
+
 # sort by length desc
 decart.sort(key=lambda x: len(x))
-print(list(map(len, decart)))
+# print(list(map(len, decart)))
 acc = decart[0]
 for i in range(1, len(decart)):
+    prop_hit = 0
     time_merge = time.time()
-    acc = merge_backdoors(acc, decart[i])
+    acc = get_unique_lists(merge_backdoors(acc, decart[i]))
     # print(acc)
     print("Time to merge: ", time.time() - time_merge)
     filtered = []
-    for j in range(len(acc)):
-        # print(j, len(decart[i]))
-        time_iter = time.time()
-        with Cadical153(bootstrap_with=formula) as solver:
+    with Cadical153(bootstrap_with=formula) as solver:
+        for j in range(len(acc)):
+            # print(j, len(decart[i]))
+            time_iter = time.time()
             solver.conf_budget(5000)
             # print(decart[i][j])
             solver.solve_limited(assumptions=acc[j])
             if solver.get_status() is None:
                 filtered.append(acc[j])
             print("Time to iteration: ", time.time() - time_iter)
-            print(len(filtered), "/",  j, "/", len(acc))
+            print(len(filtered), "/", j, "/", len(acc))
             print(solver.accum_stats())
-    strs = str((len(acc) - len(filtered)) / len(acc)) + " filtered:" + str(len(filtered)) + " acc:" + str(len(acc))
-    send_to_telegram(strs)
+    statistics = dict()
+    statistics['prop_hit'] = prop_hit
+    statistics['time'] = round(time.time() - start)
+    statistics['acc'] = len(acc)
+    statistics['filtered'] = len(filtered)
+    # format 2 digits after point sifted
+    statistics['sifted'] = round((len(acc) - len(filtered)) / len(acc), 2)
+    send_to_telegram(statistics)
     print("sifted: ", (len(acc) - len(filtered)) / len(acc), "filtered:", len(filtered), "acc:", len(acc))
     print("time from start: ", time.time() - start)
     acc = filtered
